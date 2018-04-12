70da09141d57935a487ce9e8fd46dd979d9c61ec
Jared Karpiak <jakarpiak@gmail.com>
2018-04-12 14:21:02 -0600

    added a bunch of REST stuff

--- Kung Fu Tracker/Kung_Fu_Tracker/DataManagement/RestService.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/DataManagement/RestService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Kung_Fu_Tracker.Models;
+using Newtonsoft.Json;
 
 namespace Kung_Fu_Tracker.DataManagement
 {
@@ -24,7 +25,39 @@ namespace Kung_Fu_Tracker.DataManagement
             postData.Add(new KeyValuePair<string, string>("grant_type", grant_type));
             postData.Add(new KeyValuePair<string, string>("username", user.Username));
             postData.Add(new KeyValuePair<string, string>("password", user.Password));
-
+            var weburl = "localhost";
+            var content = new FormUrlEncodedContent(postData);
+            var response = await PostResponseLogin<Token>(weburl, content);
+            DateTime dt = new DateTime();
+            dt = DateTime.Today;
+            response.expireDate = dt.AddSeconds(response.expireIn);
+            return response;
+        }
+        public async Task<T> PostResponseLogin<T>(string weburl, FormUrlEncodedContent content) where T : class
+        {
+            var response = await httpClient.PostAsync(weburl, content);
+            var jsonResult = response.Content.ReadAsStringAsync().Result;
+            var responseObject = JsonConvert.DeserializeObject<T>(jsonResult);
+            return responseObject;
+        }
+        public async Task<T> PostResponse<T>(string weburl, string jsonContent) where T : class
+        {
+            var token = App.TokenDatabase.GetToken();
+            string contentType = "application/json";
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.accessToken);
+            var result = await httpClient.PostAsync(weburl, new StringContent(jsonContent, Encoding.UTF8, contentType));
+            var jsonResult = result.Content.ReadAsStringAsync().Result;
+            var contentResp = JsonConvert.DeserializeObject<T>(jsonResult);
+            return contentResp;
+        }
+        public async Task<T> GetResponse<T>(string weburl) where T : class
+        {
+            var token = App.TokenDatabase.GetToken();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.accessToken);
+            var response = await httpClient.GetAsync(weburl);
+            var jsonResult = response.Content.ReadAsStringAsync().Result;
+            var contentResp = JsonConvert.DeserializeObject<T>(jsonResult);
+            return contentResp;
         }
     }
 }
--- Kung Fu Tracker/Kung_Fu_Tracker/Models/Token.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/Models/Token.cs
@@ -9,10 +9,10 @@ namespace Kung_Fu_Tracker.Models
     public class Token
     {
         public int Id { get; set; }
-        public string access_token { get; set; }
-        public string error_description { get; set; }
-        public DateTime expire_date { get; set; }
-
+        public string accessToken { get; set; }
+        public string errorDescription { get; set; }
+        public DateTime expireDate { get; set; }
+        public double expireIn { get; set; }
         public Token() { }
     }
 }
