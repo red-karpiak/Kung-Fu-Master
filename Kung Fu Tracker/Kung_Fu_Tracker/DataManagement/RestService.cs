29793ab7357a2e13816a3d5ed35dcca5f516d3b4
Jared Karpiak <jakarpiak@gmail.com>
2018-04-12 00:42:11 -0600

    started restapi class. but it's late and i want to sleep

---
+++ Kung Fu Tracker/Kung_Fu_Tracker/DataManagement/RestService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Kung_Fu_Tracker.Models;
+
+namespace Kung_Fu_Tracker.DataManagement
+{
+    public class RestService
+    {
+        HttpClient httpClient;
+        string grant_type = "password";
+        public RestService()
+        {
+            httpClient = new HttpClient();
+            httpClient.MaxResponseContentBufferSize = 256000;
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded' "));
+        }
+        public async Task<Token> Login(User user)
+        {
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("grant_type", grant_type));
+            postData.Add(new KeyValuePair<string, string>("username", user.Username));
+            postData.Add(new KeyValuePair<string, string>("password", user.Password));
+
+        }
+    }
+}
