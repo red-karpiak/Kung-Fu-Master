ac76d8f5248418e8e2047721bc54e7ab227c1a83
Jared Karpiak <jakarpiak@gmail.com>
2018-04-11 22:58:30 -0600

    added colors (or colours)

--- Kung Fu Tracker/Kung_Fu_Tracker/Models/Constants.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/Models/Constants.cs
@@ -10,9 +10,19 @@ namespace Kung_Fu_Tracker.Models
     public class Constants
     {
         public static bool IsDev = true;
-        public static Color BackgroundColor = Color.Black;
-        public static Color MainTextColor = Color.GhostWhite;
-        public static Color EntryColor = Color.WhiteSmoke;
+        public static Color BackgroundColor = Color.FromHex("E5E5E5");
+        public static Color MainTextColor = Color.Black;
+        public static Color WhiteTextColor = Color.WhiteSmoke;
+        public static Color EntryColor = Color.Black;
+        public static Color Red = Color.FromHex("BA0700");
+        public static Color Brown = Color.FromHex("3D1003");
+        public static Color Blue = Color.FromHex("363CC0");
+        public static Color Purple = Color.FromHex("6E0054");
+        public static Color Orange = Color.FromHex("F05209");
+        public static Color Gold = Color.FromHex("DBB214");
+        public static Color White = Color.WhiteSmoke;
         public static int LoginIconHeight = 120;
+
+        //other colors: 726F72, E9D9D9, 666265
     }
 }
