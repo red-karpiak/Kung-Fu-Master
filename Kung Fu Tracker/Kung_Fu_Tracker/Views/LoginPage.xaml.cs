47874cdf2b0df190142828f745b205cccab25676
Jared Karpiak <jakarpiak@gmail.com>
2018-04-13 22:14:58 -0600

    begin work on master detail

--- Kung Fu Tracker/Kung_Fu_Tracker/Views/LoginPage.xaml.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/Views/LoginPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Kung_Fu_Tracker.Models;
+using Kung_Fu_Tracker.Views.Menu;
 
 namespace Kung_Fu_Tracker.Views
 {
@@ -41,10 +42,21 @@ namespace Kung_Fu_Tracker.Views
             if (user.CheckInformation())
             {
                 await DisplayAlert("Login", "Login Successful", "OK");
-                var result = await App.RestService.Login(user);
-                if (result.accessToken != null)
+                //var result = await App.RestService.Login(user);
+                var result = new Token(); //dummy token for testing purposes
+               // if (result.accessToken != null)
+                if (result != null)
                 {
-                    App.UserDatabase.SaveUser(user);
+                    //App.UserDatabase.SaveUser(user);
+                    //App.TokenDatabase.SaveToken(result);
+                    if (Device.RuntimePlatform == Device.Android)
+                    {
+                        Application.Current.MainPage = new NavigationPage(new Dashboard());
+                    }
+                    else if (Device.RuntimePlatform == Device.iOS)
+                    { 
+                        await Navigation.PushAsync(new NavigationPage(new Dashboard()));
+                    }
                 }
             }
             else
