feee72738ae78f81e4cdb2e21728ea435ca665db
Jared Karpiak <jakarpiak@gmail.com>
2018-04-20 22:12:39 -0600

    datagrid column headers visible. no data yet though...

--- Kung Fu Tracker/Kung_Fu_Tracker/Views/DetailViews/PatternDetails.xaml.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/Views/DetailViews/PatternDetails.xaml.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
-using Kung_Fu_Tracker.Views;
-using Xamarin.Forms.DataGrid;
 using Xamarin.Forms.Xaml;
 
 namespace Kung_Fu_Tracker.Views.DetailViews
@@ -15,33 +13,37 @@ namespace Kung_Fu_Tracker.Views.DetailViews
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PatternDetails : ContentPage
     {
-        public DataGrid dataGrid;
-        public List<Pattern> patterns;
-        bool isRefreshing;
-        public Pattern selectedItem;
+        public List<Pattern> Patterns { get; set; }
+        bool isRefreshing = false;
+        public Pattern SelectedItem { get; set; }
         public PatternDetails(string rank)
         {
             //DataGridComponent.Init();
             InitializeComponent();
             Title = rank;
             InitGrid(rank);
+            RefreshCommand = new Command(CmdRefresh);
         }
         public void InitGrid(string rank)
         {
-            //TODO: pull the pattern steps from the db
-            patterns = new List<Pattern>();
-            patterns.Add(new Pattern(1, "White", "L: front stance", "L: low block\nR: chamber"));
-            patterns.Add(new Pattern(2, "White", "R: front stance", "L: chamber\nR: Front punch"));
-            patterns.Add(new Pattern(3, "White", "L: front kick", "L: chamber\nR: Palm to chest"));
-            patterns.Add(new Pattern(4, "White", "R: front stance", "L: chamber\nR: low block"));
-            patterns.Add(new Pattern(5, "White", "step back to R: cat stance", "L: chamber\nR: fist to L shoulder"));
-
+            //TODO: pull the pattern steps from the db. Hardcoding for testing purposes.
+            Patterns = new List<Pattern>
+            {
+                new Pattern("White", 1, "L: front stance", "L: low block\nR: chamber"),
+                new Pattern("White", 2, "R: front stance", "L: chamber\nR: Front punch"),
+                new Pattern("White", 3, "L: front kick", "L: chamber\nR: Palm to chest"),
+                new Pattern("White", 4, "R: front stance", "L: chamber\nR: low block"),
+                new Pattern("White", 5, "step back to R: cat stance", "L: chamber\nR: fist to L shoulder")
+            };
         }
+        public ICommand RefreshCommand { get; set; }
 
-        public Pattern SelectedItem
+        private async void CmdRefresh()
         {
-            get { return selectedItem; }
-            set { selectedItem = value; }
+            IsRefreshing = true;
+            // wait 3 secs for demo
+            await Task.Delay(1000);
+            IsRefreshing = false;
         }
         public bool IsRefreshing
         {
