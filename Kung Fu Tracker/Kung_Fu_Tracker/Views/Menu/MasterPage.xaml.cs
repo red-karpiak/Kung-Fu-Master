292ddab19d4b9b2724b4871b7c0429ca413358e5
Jared Karpiak <jakarpiak@gmail.com>
2018-04-26 10:17:19 -0600

    masterpage model MVVM made, beginning work on masterDetail model

--- Kung Fu Tracker/Kung_Fu_Tracker/Views/Menu/MasterPage.xaml.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/Views/Menu/MasterPage.xaml.cs
@@ -16,28 +16,13 @@ namespace Kung_Fu_Tracker.Views.Menu
     public partial class MasterPage : ContentPage
     {
         public ListView ListView { get { return lvContent; } }
-        public List<MasterMenuItem> Items { get; set; }
         public MasterPageModel masterPageModel;
         public MasterPage()
         {
             InitializeComponent();
             masterPageModel = new MasterPageModel();
             this.BindingContext = masterPageModel;
-            SetItems();
         }
-        private void SetItems()
-        {
-            Items = new List<MasterMenuItem>
-            {
-                new MasterMenuItem("User Information", "icon.png", Color.White, typeof(UserInfo)),
-                new MasterMenuItem("Patterns", "icon.png", Color.White, typeof(Patterns)),
-                new MasterMenuItem("Settings", "Settings.ico", Color.White, typeof(SettingsHome))
-            };
-        }
-        private void btnLogout_ClickedAsync(object sender, EventArgs e)
-        {
-            App.LoggedIn = false;
-            App.Current.MainPage = new LoginPage();
-        }
+        
     }
 }
--- Kung Fu Tracker/Kung_Fu_Tracker/Views/ViewModels/MasterPageModel.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/Views/ViewModels/MasterPageModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Kung_Fu_Tracker.Models;
+using Kung_Fu_Tracker.Views.DetailViews;
+using Kung_Fu_Tracker.Views.DetailViews.SettingsViews;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -12,16 +15,27 @@ namespace Kung_Fu_Tracker.Views.ViewModels
     public class MasterPageModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public List<MasterMenuItem> Items { get; set; }
         public ICommand LogoutCommand { get; set; }
         
         public MasterPageModel()
         { 
             LogoutCommand = new Command(OnLogout);
+            SetItems();
         }
         public void OnLogout()
         {
             App.LoggedIn = false;
             App.Current.MainPage = new LoginPage();
+        }
+        private void SetItems()
+        {
+            Items = new List<MasterMenuItem>
+            {
+                new MasterMenuItem("User Information", "icon.png", Color.White, typeof(UserInfo)),
+                new MasterMenuItem("Patterns", "icon.png", Color.White, typeof(Patterns)),
+                new MasterMenuItem("Settings", "Settings.ico", Color.White, typeof(SettingsHome))
+            };
         }
     }
 }
