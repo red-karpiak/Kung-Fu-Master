48f6b7c26ab917b3d1738921f720fe7656b3d8fc
Jared Karpiak <jakarpiak@gmail.com>
2018-05-01 18:20:12 -0600

    beginning web services. added ASP.NET project

--- Kung Fu Tracker/Kung_Fu_Tracker/Models/User.cs
+++ Kung Fu Tracker/Kung_Fu_Tracker/Models/User.cs
@@ -14,7 +14,7 @@ namespace Kung_Fu_Tracker.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Rank { get; set; }
-        public byte Stripes { get; set; }
+        public int Stripes { get; set; }
 
         public User() { }
         public User(string name, string pass)
